14dcac3f520c16779b46c77166f8ca13543882ea
Tarun Kumar <tarun@Taruns-MacBook-Air.local>
2024-10-15 23:06:04 +0530

    Added calculator2 file

---
+++ pythonProject/calculator2.py
@@ -0,0 +1,16 @@
+
+
+num1 = float(input("Enter the first number: "))
+operator = input("Enter the operator: ")
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator!")
