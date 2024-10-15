bf628011b3233ed1938ea006a5e748b0d1023240
Tarun Kumar <tarun@Taruns-MacBook-Air.local>
2024-10-15 23:46:20 +0530

    Added dictionary file

---
+++ pythonProject/dictionary.py
@@ -0,0 +1,18 @@
+# dictionary holds the data in a key-value pair
+from calendar import month
+
+#example
+
+month_conversion = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May"
+}
+
+# accessing the dictionary
+
+print(month_conversion["Jan"])
+print(month_conversion["Feb"])
+print(month_conversion.get("Jan"))
